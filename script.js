5601994c740bb1ac7854aa0af6b24b8be4042049
sandramunyaneza <sandramunyaneza1@gmail.com>
2026-01-09 12:47:58 +0100

    responsive1

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const btn = document.querySelector("#cacher")
-const navigation = document.querySelector("header")
+const btn = document.querySelector("#cacher");
+const navigation = document.querySelector("header");
 btn.addEventListener("click",() => navigation.classList.toggle("active"))
