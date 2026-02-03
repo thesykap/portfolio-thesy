4eebeb997d27df931bd0622d010f3fdb4230ce97
thesykap <theresekapinga047@gmail.com>
2026-02-03 16:23:34 -0500

    Mise à jour du projet

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-const btn = document.querySelector("#cacher")
+/*const btn = document.querySelector("#cacher")
 const navigation = document.querySelector("header")
 btn.addEventListener("click",() => navigation.classList.toggle("active"))
+
+
+.info-nav ul li{
+  margin-top: 0;
+}
